0f4875b405bd2b4473fded1a1d921e514c32a165
Isil Donmez <isil.donmez@gmail.com>
2018-07-25 16:48:57 +0100

    Add Auth; Add Cloud API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-import Amplify from 'aws-amplify';
-import awsExports from './src/aws-exports';
+import { View, Text, Button } from 'react-native';
+import Amplify, { API } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 class App extends Component {
-  Amplify.configure(awsExports);
+  state = { apiResponse: null };
+
+  async getSample() {
+    const path = '/items'; //you can specify the path
+    const apiResponse = await API.get('sampleCloudApi', path);
+    console.log('response:' + apiResponse);
+    this.setState({ apiResponse });
+  }
+
   render() {
     return (
-      <Text>Hello World!</Text>
+      <View>
+        <Button title="Send Request" onPress={this.getSample.bind(this)} />
+        <Text>Response: {this.state.apiResponse && JSON.stringify(this.state.apiResponse)}</Text>
+        <Text>Hello World!</Text>
+      </View>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App);
